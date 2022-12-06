5f8133df5ac5988bd036a5a3454a7fdc4f6f8ab5
mradawiec <milosz523@o2.pl>
2022-12-06 21:41:20 +0100

    First task ended successfully

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,3 @@
 public class Human {
-    Animal Pet = new Animal();
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        Animal dog = new Animal("canis");
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
     }
 }
