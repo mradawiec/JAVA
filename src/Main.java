3263a14c8467448817263012ea4f1a5982e1204f
mradawiec <milosz523@o2.pl>
2022-12-07 19:39:37 +0100

    First second ended successfully

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Animal dog = new Animal("canis");
         dog.takeForAWalk();
-        dog.takeForAWalk();
-        dog.takeForAWalk();
+        dog.feed();
+
+        Car car = new Car("BMW","E36", "Nardo Grey", 1.6);
+        car.newRide();
     }
 }
