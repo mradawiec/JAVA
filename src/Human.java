e377c3de6207c7e6793bd27400254ac17ecc8543
mradawiec <milosz523@o2.pl>
2022-12-08 19:18:21 +0100

    Third task ended successfully! Minimal wage has been achieved ;)

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,28 @@
+import java.util.Date;
 public class Human {
+    Animal pet;
+    Car car;
+    Human moneyFromJob;
+    Date dateOfChange = new Date();
+    private Double salary;
+    public Double getSalary() {
+        System.out.println("Last info about seeing your salary was: " +dateOfChange+ "and salary was: "+ salary + "\n");
+        return salary;
+    }
+    public void setSalary(Double salary) {
+        if(salary>0) {
+            this.salary = salary;
+            System.out.println("New salary sent to HR department");
+            System.out.println("Need to collect an annex to the contract from Mrs. Hania of dept. HR");
+            System.out.println("ZUS and US already now about new salary");
+            System.out.println("Well done, your new salary is: " + salary + "\n");
+        }else
+        {
+            System.out.println("New salary must be higher than 0 pesos" + "\n");
+        }
+
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         Animal dog = new Animal("canis");
-        dog.takeForAWalk();
-        dog.feed();
+        Car ride = new Car("BMW","E36", "Nardo Grey", 1.6);
+        Human jobFunds = new Human();
 
-        Car car = new Car("BMW","E36", "Nardo Grey", 1.6);
-        car.newRide();
+        Human Marek = new Human();
+        Marek.pet=dog;
+        Marek.pet.takeForAWalk();
+        Marek.pet.feed();
+
+        ride.newRide();
+        Marek.car=ride;
+
+        Marek.moneyFromJob=jobFunds;
+        jobFunds.setSalary(3000.69);
+        jobFunds.getSalary();
+        jobFunds.setSalary(4001.69);
+        jobFunds.getSalary();
+
     }
 }
